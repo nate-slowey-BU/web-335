2131f782188b4feaba98108aa6be48f33219c100
nate-slowey-BU <nslowey@my365.bellevue.edu>
2025-01-16 20:12:26 -0600

    week 2 python

---
+++ week_2/1-1.py
@@ -0,0 +1,9 @@
+'''
+Author: Nate Slowey
+Date: 1/16
+File Name: 1-1.py
+Description: Example file for outputting values.
+'''
+
+# Printing the quote 
+print("The only thing we have to fear is fear itself - Franklin D. Roosevelt")
